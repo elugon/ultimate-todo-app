6fad36ada50dbf42cc374ce4a124a445cb1eacee
Elu Gonzalez <elugon10@gmail.com>
2022-08-14 20:19:48 +0200

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import tasks from './list.json';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="todo-app">
+      <TodoList />
     </div>
   );
 }
